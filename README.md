f99a7082b75a51e9447e7fd8cc0c25c698d598c4
Roy Feintuch <froyke@gmail.com>
2015-02-02 20:48:35 -0800

    Adding Express to serve local folders
    Now config file contains ‘static’ filed that allows to serve entire
    folders
    
    The ‘old’ routes are still there as they allow to serve a specific file
    and allow to route based on query string as well.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # dev-proxy
 
-a ~ 20 LOC reverse-proxy server (based on http-proxy) to assit the front-end developer in these use-cases:
+a < 50 LOC reverse-proxy server (based on http-proxy) to assit the front-end developer in these use-cases:
 
-1. Simulate and Mock data while working on a UI feature. (without god forbid modifying the application code and forgetting it there)
+1. Simple web server to serve your local files
+2. Simulate and Mock data while working on a UI feature. (without god forbid modifying the application code and forgetting it there)
 2. Start working on a feature way before the backend dudes have given you something.
-3. (Future) Working on a local version of the application while connecting to test/staging/prod backend, without messing with CORS. 
+3. Working on a local version of the application while connecting to test/staging/prod backend, without messing with CORS. 
 
-This proxy will let you override some responses for specific paths, while proxying all other requests to the real origin server.
+This server will let you serve your local application file while proxying the backend requests to an origin server of your choice.
+It will also allow you to override some responses for specific paths (so you could easily mimic some application cases / work with not-yet-created-api).
 
-Look at the config.json file to understand how to provide custom routes, and configure the origin server and ports.
+Take a look at the config.json file to understand how to provide custom routes, and configure the origin server and ports.
 
 Instructions:
 - git clone this repo
--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 var http = require('http'),
 	httpProxy = require('http-proxy'),
 	fs = require('fs'),
-	_ = require('lodash');
+	_ = require('lodash'),
+	express = require('express'),
+	app = express(),
+	path = require('path');
 
-//
-// Create a proxy server with custom application logic
-//
 var proxy = httpProxy.createProxyServer({});
 var config = require("./config.json");
 
-console.log("Starting dev-proxy , Config=",config);
-
-var server = http.createServer(function(req, res) {
-  	console.log("Request for: " + req.url);
-
- 	var configured_route = _.find(config.routes, function(route){return req.url.indexOf(route.route) >-1;});
-	if(configured_route ){
-		console.log("Serving local file: ",configured_route.path );
-		var fileStream = fs.createReadStream(configured_route.path);
-		fileStream.pipe(res);
-	}
-	else{
-		req.headers["X-FORWARDED-FOR"] = req.connection.remoteAddress; 
-		proxy.web(req, res, { target: config.origin_server});
-	}
+app.use(function(req,res,next){
+	console.log("Request: " + req.url);
+	next();
 });
 
-console.log("Proxy listening on port: %s, origin server is: %s" , config.proxy_port, config.origin_server);
-server.listen(config.proxy_port);
+// register static files with Express statoic middleware. This will also handle sub-directories
+_.each(config.static, 
+	function(route){
+		app.use(route.route, express.static(route.path));
+	});
 
+// Serve specific routes wil local files. This is different from 'static' since it can handle query strings and is defined for a single file.
+// It is intended to replace some backend server response
+app.use(function(req, res, next) {
+ 	var configured_route = _.find(config.routes, function(route){
+ 		return req.url.indexOf(route.route) >-1;});
+	if(configured_route ){
+		var absolute_path = configured_route.path.indexOf('/')==0 ? configured_route.path : __dirname + "/" + configured_route.path; // we allow absolute or relative paths (that do not start with /)
+		console.log("*** local override : ",absolute_path);
+		res.sendFile(absolute_path ); 
+	}
+	else
+		next();
+});
 
+// Last handler is to proxy all unhandled requests to origin server
+app.use(function(req,res,next){
+	console.log("* proxying to origin");
+	req.headers["X-FORWARDED-FOR"] = req.connection.remoteAddress; 
+	proxy.web(req, res, { target: config.origin_server});
+});
+
+console.log("Starting dev-proxy , Config=",config);
+console.log("Proxy listening on port: %s, origin server is: %s" , config.port, config.origin_server);
+var server = http.createServer(app);
+server.listen(config.port);
 
 /*
 // A dummy origin server (to assist dev-proxy development)
